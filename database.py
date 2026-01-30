5a4bdb13162b37a4e682bc8fffc87b911c576a94
PatDagos96 <114470929+PatDagos96@users.noreply.github.com>
2026-01-30 10:22:58 +0100

    Add files via upload

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 # Sostituisci la stringa qui sotto con quella che hai copiato da Neon.tech
 # Deve iniziare con postgres:// e finire con .neon.tech/neondb... (o simile)
 # IMPORTANTE: Se la stringa inizia con "postgres://", cambiala in "postgresql://" (aggiungi la 'ql')
-DATABASE_URL = "npx neonctl@latest init"
+DATABASE_URL = "postgresql://neondb_owner:npg_7PECzDsQx1lT@ep-gentle-meadow-ahzxk371-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'"
 
 # Correzione automatica per un piccolo bug di compatibilità (se serve)
 if DATABASE_URL.startswith("postgres://"):
